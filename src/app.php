e6408aefbc828d92f6543c867b0feaf78a07b046
Giustino Borzacchiello <giustinob@gmail.com>
2014-08-31 00:12:45 +0200

    registered url provider

--- src/app.php
+++ src/app.php
@@ -2,9 +2,11 @@
 
 use Silex\Application;
 use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\UrlGeneratorServiceProvider;
 
 $app = new Application();
 $app->register(new TwigServiceProvider());
+$app->register(new UrlGeneratorServiceProvider());
 
 return $app;
 
