8762ced569463efc4872479a94991144eb7fca59
Giustino Borzacchiello <giustinob@gmail.com>
2014-08-31 11:41:54 +0200

    Added Url generator service

--- src/controllers.php
+++ src/controllers.php
@@ -4,5 +4,10 @@ $app->get('/', function() use ($app) {
     return $app['twig']->render('hello.twig', array(
         'name' => 'World',
     ));
+})
+->bind('home');
 
-});
+$app->get('/goodbye', function() use ($app) {
+
+    return $app['twig']->render('goodbye.twig');
+})->bind('goodbye');
