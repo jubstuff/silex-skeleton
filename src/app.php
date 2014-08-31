8a62140eaa0319e1af7142b65d171baec1d49ce8
Giustino Borzacchiello <giustinob@gmail.com>
2014-08-31 12:51:58 +0200

    Display contact form in the contact page

--- src/app.php
+++ src/app.php
@@ -3,10 +3,15 @@
 use Silex\Application;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
+use Silex\Provider\FormServiceProvider;
+use Silex\Provider\TranslationServiceProvider;
 
 $app = new Application();
+$app['debug'] = true;
 $app->register(new TwigServiceProvider());
 $app->register(new UrlGeneratorServiceProvider());
+$app->register(new FormServiceProvider());
+$app->register(new TranslationServiceProvider());
 
 return $app;
 
--- src/controllers.php
+++ src/controllers.php
@@ -1,4 +1,5 @@
 <?php
+use Symfony\Component\HttpFoundation\Request;
 
 $app->get('/', function() use ($app) {
     return $app['twig']->render('home.twig');
@@ -9,6 +10,28 @@ $app->get('/about', function() use ($app) {
     return $app['twig']->render('about.twig');
 })->bind('about');
 
-$app->get('/contact', function() use ($app) {
-    return $app['twig']->render('contact.twig');
+$app->match('/contact', function(Request $request) use ($app) {
+    $data = array(
+        'name' => 'Your name',
+        'email' => 'Your email',
+        'message' => 'Your message',
+    );
+
+    $form = $app['form.factory']->createBuilder('form', $data)
+        ->add('name')
+        ->add('email')
+        ->add('message')
+        ->getForm();
+
+    $form->handleRequest($request);
+
+    if($form->isValid()) {
+        $data = $form->getData();
+
+        //TODO manage form data
+
+        return $app->redirect('/');
+    }
+
+    return $app['twig']->render('contact.twig', array('form' => $form->createView()));
 })->bind('contact');
